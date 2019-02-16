e20fd4e02982ed6247d400ce581849462e6999f4
Aravind Sankaran <aravind.sankaran@rwth-aachen.de>
2019-02-17 01:06:48 +0530

    Added directives, server context

--- nginx/test_run.sh
+++ nginx/test_run.sh
@@ -1,1 +1,5 @@
-docker run -it -p 80:80 nginx bash
+docker run -it -p 80:80 \
+    -v ${PWD}/html/:/usr/share/nginx/html \
+    -v ${PWD}/confs/nginx.conf:/etc/nginx/nginx.conf \
+    -v ${PWD}/confs/conf.d/:/etc/nginx/conf.d \
+     nginx bash
